04dff7d153d3c580cccc79d042709709fee78765
Alex <alexscott200020@gmail.com>
2025-05-28 00:08:18 +1200

    update routes

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\MoodTrackingController;
 use App\Http\Controllers\SleepLogController;
 use App\Http\Controllers\GoalSettingController;
 use App\Http\Controllers\ExerciseController;
+use App\Http\Controllers\HealthInsightsController;
 
 // Home Routes
 Route::get('/', function () {
@@ -23,15 +24,8 @@ Route::get('/home', function () {
 Route::get('/support-resources', [SupportResourceController::class, 'index'])->name('support-resources.index');
 
 // Authentication Routes
-
-Route::get('/signup', function () {
-    return view('authentication.signup');
-});
-
-Route::get('/signin', function () {
-    return view('authentication.signin');
-});
-
+Route::get('/signup', [UserController::class, 'showSignupForm'])->name('signup');
+Route::get('/signin', [UserController::class, 'showSigninForm'])->name('signin');
 Route::post('/signup', [UserController::class, 'signup'])->name('register');
 Route::post('/logout', [UserController::class, 'logout'])->name('logout');
 Route::post('/signin', [UserController::class, 'signin'])->name('login');
@@ -41,9 +35,11 @@ Route::post('/signin', [UserController::class, 'signin'])->name('login');
 Route::middleware(['auth'])->group(function () {
 
     // Account Routes
-    Route::get('/account', [UserController::class, 'showAccount'])->name('account.show');
-    Route::post('/account/edit', [UserController::class, 'editAccount'])->name('account.edit');
-    Route::post('/account/delete', [UserController::class, 'deleteAccount'])->name('account.delete');
+    Route::prefix('account')->as('account.')->group(function () {
+        Route::get('/', [UserController::class, 'showAccount'])->name('show');
+        Route::get('/edit', [UserController::class, 'editAccountPage'])->name('edit');
+        Route::put('/edit', [UserController::class, 'updateAccount'])->name('update');
+    });
 
     // Admin-only routes
     Route::middleware('IsAdmin')->prefix('admin')->as('admin.')->group(function () {
@@ -63,68 +59,65 @@ Route::middleware(['auth'])->group(function () {
     });
 
     //  Mood Routes
-    Route::get('/mood', [MoodTrackingController::class, 'index'])->name('mood.index');
-    Route::get('/mood/track-mood', [MoodTrackingController::class, 'track'])->name('mood.track');
-    Route::post('/mood/track-mood', [MoodTrackingController::class, 'store'])->name('mood.store');
-    Route::get('/mood/edit-mood/{id}', [MoodTrackingController::class, 'edit'])->name('mood.edit');
-    Route::put('/mood/edit-mood/{id}', [MoodTrackingController::class, 'update'])->name('mood.update');
+    Route::prefix('mood')->as('mood.')->group(function () {
+        Route::get('/', [MoodTrackingController::class, 'index'])->name('index');
+        Route::get('/track-mood', [MoodTrackingController::class, 'track'])->name('track');
+        Route::post('/track-mood', [MoodTrackingController::class, 'store'])->name('store');
+        Route::get('/edit-mood/{id}', [MoodTrackingController::class, 'edit'])->name('edit');
+        Route::put('/edit-mood/{id}', [MoodTrackingController::class, 'update'])->name('update');
+    });
 
     // Exercise Routes
-    Route::get('/exercise', [ExerciseController::class, 'index'])->name('exercise.index');
-    Route::get('/exercise/plan-exercise', [ExerciseController::class, 'addPlannedExercisePage'])->name('exercise.plan');
-    Route::post('/exercise/plan-exercise', [ExerciseController::class, 'storePlannedExercise'])->name('exercise.store.plan');
-    Route::get('/log-exercise/{plannedExerciseID}', [ExerciseController::class, 'logExercisePage'])->name('exercise.log');
-    Route::get('/log-exercise', [ExerciseController::class, 'logExercisePage'])->name('exercise.log.unplanned');
-    Route::post('/log-exercise/{plannedExerciseID?}', [ExerciseController::class, 'storeLoggedExercise'])->name('exercise.store.log');
-
-    Route::get('/exercise/edit-exercise/{plannedExerciseID}', [ExerciseController::class, 'editPlannedExercisePage'])->name('exercise.edit');
-    Route::put('/exercise/edit-exercise/{plannedExerciseID}', [ExerciseController::class, 'updatePlannedExercise'])->name('exercise.update');
+    Route::prefix('exercise')->group(function () {
+        Route::get('/', [ExerciseController::class, 'index'])->name('exercise.index');
+        Route::get('/plan-exercise', [ExerciseController::class, 'addPlannedExercisePage'])->name('exercise.plan');
+        Route::post('/plan-exercise', [ExerciseController::class, 'storePlannedExercise'])->name('exercise.store.plan');
+        Route::get('/log-exercise/{plannedExerciseID}', [ExerciseController::class, 'logExercisePage'])->name('exercise.log');
+        Route::get('/log-exercise', [ExerciseController::class, 'logExercisePage'])->name('exercise.log.unplanned');
+        Route::post('/log-exercise/{plannedExerciseID?}', [ExerciseController::class, 'storeLoggedExercise'])->name('exercise.store.log');
+        Route::get('/edit-exercise/{plannedExerciseID}', [ExerciseController::class, 'editPlannedExercisePage'])->name('exercise.edit');
+        Route::put('/edit-exercise/{plannedExerciseID}', [ExerciseController::class, 'updatePlannedExercise'])->name('exercise.update');
+    });
 
     // Goals Routes
-    Route::get('/goals', [GoalSettingController::class, 'index'])->name('goals.index');
-    Route::get('/goals/log-goal/{id}', [GoalSettingController::class, 'log'])->name('goals.log');
-    Route::post('/goals/log-goal/{id}', [GoalSettingController::class, 'storeLog'])->name('goals.store.log');
-    Route::get('/goals/edit-goal/{id}', [GoalSettingController::class, 'edit'])->name('goals.edit');
-    Route::put('/goals/edit-goal/{id}', [GoalSettingController::class, 'update'])->name('goals.update');
-    Route::get('/goals/set-goal', [GoalSettingController::class, 'set'])->name('goals.set');
-    Route::post('/goals/set-goal', [GoalSettingController::class, 'store'])->name('goals.store.set');
+    Route::prefix('goals')->group(function () {
+        Route::get('/', [GoalSettingController::class, 'index'])->name('goals.index');
+        Route::get('/log-goal/{id}', [GoalSettingController::class, 'log'])->name('goals.log');
+        Route::post('/log-goal/{id}', [GoalSettingController::class, 'storeLog'])->name('goals.store.log');
+        Route::get('/edit-goal/{id}', [GoalSettingController::class, 'edit'])->name('goals.edit');
+        Route::put('/edit-goal/{id}', [GoalSettingController::class, 'update'])->name('goals.update');
+        Route::get('/set-goal', [GoalSettingController::class, 'set'])->name('goals.set');
+        Route::post('/set-goal', [GoalSettingController::class, 'store'])->name('goals.store.set');
+    });
 
     // Sleep Routes
-    Route::get('/sleep', [SleepLogController::class, 'index'])->name('sleep.index');
-    Route::get('/sleep/log-sleep', [SleepLogController::class, 'log'])->name('sleep.log');
-    Route::post('/sleep/log-sleep', [SleepLogController::class, 'store'])->name('sleep.store');
-    Route::get('/sleep/edit-sleep/{id}', [SleepLogController::class, 'edit'])->name('sleep.edit');
-    Route::put('/sleep/edit-sleep/{id}', [SleepLogController::class, 'update'])->name('sleep.update');
+    Route::prefix('sleep')->group(function () {
+        Route::get('/', [SleepLogController::class, 'index'])->name('sleep.index');
+        Route::get('/log-sleep', [SleepLogController::class, 'log'])->name('sleep.log');
+        Route::post('/log-sleep', [SleepLogController::class, 'store'])->name('sleep.store');
+        Route::get('/edit-sleep/{id}', [SleepLogController::class, 'edit'])->name('sleep.edit');
+        Route::put('/edit-sleep/{id}', [SleepLogController::class, 'update'])->name('sleep.update');
+    });
 
     // Health Insights Routes
-    Route::get('/health-insights', function () {
-        return view('health-insights.health-insights');
-    });
-
-    Route::get('/health-insights/mood', function () {
-        return view('health-insights.mood-insights');
-    });
-
-    Route::get('/health-insights/exercise', function () {
-        return view('health-insights.exercise-insights');
-    });
-
-    Route::get('/health-insights/sleep', function () {
-        return view('health-insights.sleep-insights');
-    });
-
-    Route::get('/health-insights/goals', function () {
-        return view('health-insights.goals-insights');
+    Route::prefix('health-insights')->group(function () {
+        Route::get('/', [HealthInsightsController::class, 'index'])->name('health-insights.index');
+        Route::get('/mood', [HealthInsightsController::class, 'mood'])->name('health-insights.mood');
+        Route::get('/exercise', [HealthInsightsController::class, 'exercise'])->name('health-insights.exercise');
+        Route::get('/sleep', [HealthInsightsController::class, 'sleep'])->name('health-insights.sleep');
+        Route::get('/goals', [HealthInsightsController::class, 'goals'])->name('health-insights.goals');
     });
 
     // Forum Routes
-    Route::get('/forum', [ForumController::class, 'index'])->name('forum.index');
-    Route::get('/forum/create', [ForumController::class, 'create'])->name('forum.create');
-    Route::post('/forum', [ForumController::class, 'store'])->name('forum.store');
-    Route::get('/forum/post/{id}', [ForumController::class, 'show'])->name('forum.show');
-    Route::post('/forum/post/{id}/reply', [ForumController::class, 'reply'])->name('forum.reply');
-    Route::post('/forum/post/{forum_post}/like', [ForumController::class, 'likePost'])->name('forum.like.post');
-    Route::post('/forum/reply/{forum_reply}/like', [ForumController::class, 'likeReply'])->name('forum.like.reply');
-    Route::delete('/forum/post/{id}', [ForumController::class, 'delete'])->name('forum.delete');
-    Route::delete('/forum/reply/{id}', [ForumController::class, 'deleteReply'])->name('forum.delete.reply');
+    Route::prefix('forum')->group(function () {
+        Route::get('/', [ForumController::class, 'index'])->name('forum.index');
+        Route::get('/create', [ForumController::class, 'create'])->name('forum.create');
+        Route::post('/', [ForumController::class, 'store'])->name('forum.store');
+        Route::get('/post/{id}', [ForumController::class, 'show'])->name('forum.show');
+        Route::post('/post/{id}/reply', [ForumController::class, 'reply'])->name('forum.reply');
+        Route::post('/post/{forum_post}/like', [ForumController::class, 'likePost'])->name('forum.like.post');
+        Route::post('/reply/{forum_reply}/like', [ForumController::class, 'likeReply'])->name('forum.like.reply');
+        Route::delete('/post/{id}', [ForumController::class, 'delete'])->name('forum.delete');
+        Route::delete('/reply/{id}', [ForumController::class, 'deleteReply'])->name('forum.delete.reply');
+    });
 });
